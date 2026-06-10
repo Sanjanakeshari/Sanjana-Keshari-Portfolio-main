b6e0ec7a3be1d2a1c39b3a3a5804b13b1fca01d1
Sanjana Keshari <147981954+Sanjanakeshari@users.noreply.github.com>
2026-06-10 22:45:00 +0530

    updated projects

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -24,14 +24,6 @@ description:
 github: "https://github.com/Sanjanakeshari/Sorting_Algorithm_Visualizer",
 live: "https://sorting-algorithm-visualizer-ten-lovat.vercel.app/",
 },
-  {
-title: "Student Performance Analytics Dashboard",
-description:
-"Created An interactive dashboard that helps educational institutions identify at-risk students by analyzing marks, attendance, and login behavior.Institutions often need early warning systems to identify students who might fail or drop out.
-This project analyzes student data to highlight Performance trends, Risk areas, Impact of absenteeism",
-github: "https://github.com/Sanjanakeshari/StudentPerformance_AnalyticsDashboard",
-live: "#",
-},
   
 ];
 
